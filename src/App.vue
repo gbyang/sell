1e1d0808571c6752762e0ecf626a2e835fb50c94
gbyang <15889607783@163.com>
2017-11-09 16:24:28 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-one">
+      <div class="tab-item">
+        <router-link :to="{ path: '/goods' }">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link :to="{ path: '/ratings' }">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link :to="{ path: '/seller' }">商家</router-link>
+      </div>
+    </div>
+    <keep-alive>
+      <router-view :seller="seller"></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header'
+  import {urlParse} from './common/js/util'
+
+  const ERR_OK = 0
+
+  export default {
+    data () {
+      return {
+        seller: {
+          id: (() => {
+            let queryParam = urlParse()
+            return queryParam.id
+          })()
+        }
+      }
+    },
+    created () {
+      this.$http.get('/api/seller?id=' + this.seller.id)
+        .then((res) => {
+          if (res.data.errno === ERR_OK) {
+//            this.seller = res.data.data
+            this.seller = Object.assign({}, this.seller, res.data.data)
+//            console.log(this.seller.id)
+          }
+        })
+    },
+    components: {
+      'v-header': header
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+
+  .tab
+    display:flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7,17,27,0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77,85,93)
+        &.router-link-active
+          color: rgb(240,20,20)
+</style>
---
+++ src/components/cartcontrol/cartcontrol.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="cartcontrol">
+    <transition name="move">
+      <div class="cart-decrease" v-show="food.count > 0" @click.stop.prevent="decreaseCart">
+          <i class="inner icon-remove_circle_outline"></i>
+      </div>
+    </transition>
+    <div class="cart-count"  v-show="food.count > 0">{{ food.count }}</div>
+    <div class="cart-add icon-add_circle" @click.stop.prevent="addCart"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      food: {
+        type: Object
+      }
+    },
+    methods: {
+      addCart () {
+        if (!this.food.count) {
+          this.$set(this.food, 'count', 1)
+          this.food.count = 1
+        } else {
+          this.food.count ++
+        }
+        this.$emit('add', event.target)
+      },
+      decreaseCart () {
+        if (this.food.count > 0) {
+          this.food.count --
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .cartcontrol
+    font-size: 0
+    .cart-decrease
+      display: inline-block
+      padding: 6px
+      &.move-enter-active, &.move-leave-active
+        transition: all 0.5s ease
+      &.move-enter, &.move-leave-to
+        opacity: 0
+        transform: translateX(36px) rotate(180deg)
+      .inner
+        line-height: 24px
+        font-size: 24px
+        color: rgb(0,160,220)
+    .cart-count
+      display: inline-block
+      vertical-align: top
+      width: 12px
+      padding-top: 6px
+      line-height: 24px
+      text-align: center
+      font-size: 10px
+      color: rgb(147,153,159)
+    .cart-add
+      display: inline-block
+      padding: 6px
+      line-height: 24px
+      font-size: 24px
+      color: rgb(0,160,220)
+
+</style>
+
